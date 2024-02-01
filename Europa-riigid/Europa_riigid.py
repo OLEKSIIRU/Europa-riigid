78dcaa606b86aec254a963f142a1573d2f9f6bd0
opilane <opilane@HP-CZC2349HTY>
2024-02-01 11:32:29 +0200

    Add project files.

---
+++ Europa-riigid/Europa_riigid.py
@@ -0,0 +1,43 @@
+﻿
+
+from Function import show, save, game, find, add, edit
+
+
+
+def main():
+    riik_pealinn_dict, pealinn_riik_dict, riigid_list = failist_to_dict('Europa.txt')
+    riik_pealinn = {}  
+
+    while True:
+        print("1: Отобразить словарь")
+        print("2: Поиск столицы/страны")
+        print("3: Добавить слово в словарь")
+        print("4: Исправить ошибку в словаре")
+        print("5: Проверить знание слов")
+        print("6: Сохранить словарь в файл")
+        print("0: Выйти")
+
+        choice = input("Выберите действие: ")
+
+        if choice == "1":
+            show(riik_pealinn)
+        elif choice == "2":
+            find(riik_pealinn)
+        elif choice == "3":
+            word_to_add = input("Введите слово для добавления в словарь: ").capitalize()
+            add(riik_pealinn, word_to_add)
+        elif choice == "4":
+            edit(riik_pealinn)
+        elif choice == "5":
+            game(riik_pealinn)
+        elif choice == "6":
+            save(riik_pealinn)
+        elif choice == "0":
+            print("Программа завершена.")
+            break
+        else:
+            print("Неверный выбор. Пожалуйста, выберите снова.")
+
+if __name__ == "__main__":
+    main()
+
---
+++ Europa-riigid/Function.py
@@ -0,0 +1,118 @@
+﻿from random import *
+
+def failist_to_dict(f:str):
+    riik_pealinn={}
+    pealinn_riik={}
+    riigid=[]
+    file=open(f,'r',encoding="utf8")
+    for line in file:
+        k,v=line.strip().split('-')
+        riik_pealinn[k]=v
+        pealinn_riik=[v]=k
+        riigid.append(k)
+    file.close()
+    return riik_pealinn,pealinn_riik,riigid
+
+
+def show():
+    i = 0
+    for x in riik_pealinn:
+        i += 1
+        print(f"{i}: {x} - {riik_pealinn[x]}")
+
+    print()
+
+def save():
+    max_len = len(list(riik_pealinn.keys()))
+    with open("Europa.txt", 'w') as f:
+        f.write("")
+    
+    with open("Europa.txt", 'a') as f:
+        for x in range(max_len):
+            f.write(f"{list(riik_pealinn.keys())[x]}-{list(riik_pealinn.values())[x]}\n")
+
+def game():
+    win = 0
+    max_len = len(list(riik_pealinn.keys()))
+    
+    for x in range(max_len):
+        r = randint(0, max_len - 1)
+
+        if randint(0, 5000) % 2 == 0:
+            print("Riik = Pealinn?")
+            rp = list(riik_pealinn.keys())[r]
+        else:
+            print("Pealinn = Riik?")
+            rp = list(riik_pealinn.values())[r]
+
+        answer = str(input(f"{rp}=")).capitalize()
+
+        if riik_pealinn[list(riik_pealinn.keys())[r]] == answer or \
+           list(riik_pealinn.values())[r] == answer:
+            win += 1
+            print("Правильно")
+        else:
+            print("Неверно")
+
+    print(f"{(win / max_len) * 100}% ваш результат")
+    print()
+
+def find():
+    sona = str(input("Введите слово: ")).capitalize()
+
+    if sona in riik_pealinn:
+        print(f"{sona} -> {riik_pealinn[sona]}")
+    elif sona in riik_pealinn.values():
+        
+        riik = [key for key, value in riik_pealinn.items() if value == sona][0]
+        print(f"{sona} -> {riik}")
+    else:
+        YN = str(input("Хотите добавить это слово в словарь? (y/n): "))
+        if YN[0].lower() == "y":
+            add(sona)
+
+def add(sona):
+    if sona != "":
+        while True:
+            option = str(input("R/l: ")).lower()
+            if option[0] == "r":
+                r = sona
+                break
+            elif option[0] == "l":
+                l = sona
+                break
+
+        while True:
+            if option[0] != "r":
+                riik = str(input("Страна?: ")).capitalize()
+            elif option[0] != "l":
+                pealinn = str(input("Столица: ")).capitalize()
+            print("\nВсе верно?")
+            option = input("Введите 'v' для подтверждения, 'q' для выхода: ").lower()
+            if option == "v":
+                riik_pealinn[riik] = pealinn
+                break
+            elif option == "q":
+                break
+
+def edit():
+    show()
+    max_len = len(list(riik_pealinn.keys()))
+    pick = 0
+    while not (0 < pick <= max_len):
+        pick1 = str(input("Строка: "))
+        if pick1[0].lower() == "q":
+            break
+        if pick1.isnumeric():
+            pick = int(pick1)
+
+    lrp = list(riik_pealinn.keys())[pick - 1]
+    op = str(input("Тест: ")).capitalize()
+    while True:
+        if op[0].lower() == 'l':
+            nimi = str(input("Тест: ")).capitalize()
+            riik_pealinn[lrp] = nimi
+            break
+        elif op[0].lower() == 'r':
+            riik = str(input("Страна: ")).capitalize()
+            break
